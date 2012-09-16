db4bc9ac845462e6864ced285aa46e9a05cd0704
Scott Vijay Gehlot <scott.vijay.gehlot@gmail.com>
2012-09-16 16:12:41 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Billmurray::Application.config.secret_token = '1964757d3a84458c87d23afb3bfff24f11a110bcae2887f6daf35fc7548e7f790153b64ade2924d5ac05bb4f45c0740bfda36dcfce65c1f89eddc8d57feac788'
